56b512c7e4dd6fe8bf06737b4bc595ff53e4f00d
Joshim-Uddin <mju881@gmail.com>
2024-06-01 21:34:31 +0600

    collection page responsive done

--- src/Pages/Collection.jsx
+++ src/Pages/Collection.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import heroImage from "./../assets/dolce.png"
+import heromobile from "./../assets/dolcemobile.png"
 import Pagination from '../Components/Pagination';
 import { Link } from 'react-router-dom';
 
@@ -66,88 +67,89 @@ const handleFilter=(e)=>{
     console.log(filteredProducts, currentProducts, totalPages)
 
 }
-console.log(totalPages)
+
 
     return (
         <div>
             <div>
-                <img src={heroImage} alt="" className='w-full'/>
+                <img src={heroImage} alt="" className='w-full max-[600px]:hidden'/>
+                <img src={heromobile} alt="" className='w-full min-[600px]:hidden'/>
             </div>
-            <div className="products px-32">
+            <div className="products md:px-32 px-6">
                 <div className='flex justify-between pt-28'>
-                <div className='charm text-3xl cursor-pointer' onClick={()=>setFilter(!filter)}>
+                <div className='charm md:text-3xl text-[15px] cursor-pointer' onClick={()=>setFilter(!filter)}>
                 <input type="button" value="Filtros"  /> <span>{filter?"▲":"▼"}</span>
                 </div>
-                <p className='charm text-3xl'>Ayudame a encontrar mi aroma ⓘ</p>
+                <p className='charm md:text-3xl text-[15px]'>Ayudame a encontrar mi aroma ⓘ</p>
                 </div>
                 <div className={filter?"flex justify-between":""}>
-                <div className={filter?'filter block z-30 mt-24':'filter hidden'}>
-    <div>
-        <h3 className='text-4xl mb-5 capitalize'>Colecciones</h3>
-        <div className="check">
-        <input type="checkbox" name="Femenino" id="Femenino" onClick={handleFilter}/><label htmlFor="Femenino">Femenino</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="Maculino" id="Maculino" onClick={handleFilter}/>
-        <label htmlFor="Maculino">Maculino</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="Neutral" id="Neutral" onClick={handleFilter}/>
-        <label htmlFor="Neutral">Neutral</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="Alta Gama" id="Alta Gama" onClick={handleFilter}/>
-        <label htmlFor="Alta Gama">Alta Gama</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="árabes" id="árabes" onClick={handleFilter}/>
-        <label htmlFor="árabes">árabes</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="Diseñador" id="Diseñador" onClick={handleFilter}/>
-        <label htmlFor="Diseñador">Diseñador</label>
-        </div>
-    </div>
-    <div className='mt-16'>
-        <h3 className='text-4xl mb-5 capitalize'>Notas</h3>
-        <div className="check">
-        <input type="checkbox" name="frutales" id="frutales" /><label htmlFor="frutales">frutales</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="florales" id="florales" />
-        <label htmlFor="florales">florales</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="herbales" id="herbales" />
-        <label htmlFor="herbales">herbales</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="maderosas" id="maderosas" />
-        <label htmlFor="maderosas">maderosas</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="sensuales" id="sensuales" />
-        <label htmlFor="sensuales">sensuales</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="especiadas" id="especiadas" />
-        <label htmlFor="especiadas">especiadas</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="dulces" id="dulces" />
-        <label htmlFor="dulces">dulces</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="cítricas" id="cítricas" />
-        <label htmlFor="cítricas">cítricas</label>
-        </div>
-        <div className="check">
-        <input type="checkbox" name="balsamos" id="balsamos" />
-        <label htmlFor="balsamos">balsamos</label>
-        </div>
-    </div>
+                <div className={filter?'filter block z-30 md:mt-24 mt-8':'filter hidden'}>
+                    <div>
+                        <h3 className='md:text-4xl text-lg mb-5 capitalize'>Colecciones</h3>
+                        <div className="check">
+                        <input type="checkbox" name="Femenino" id="Femenino" onClick={handleFilter}/><label htmlFor="Femenino">Femenino</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="Maculino" id="Maculino" onClick={handleFilter}/>
+                        <label htmlFor="Maculino">Maculino</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="Neutral" id="Neutral" onClick={handleFilter}/>
+                        <label htmlFor="Neutral">Neutral</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="Alta Gama" id="Alta Gama" onClick={handleFilter}/>
+                        <label htmlFor="Alta Gama">Alta Gama</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="árabes" id="árabes" onClick={handleFilter}/>
+                        <label htmlFor="árabes">árabes</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="Diseñador" id="Diseñador" onClick={handleFilter}/>
+                        <label htmlFor="Diseñador">Diseñador</label>
+                        </div>
+                    </div>
+                    <div className='mt-16'>
+                        <h3 className='md:text-4xl text-lg mb-5 capitalize'>Notas</h3>
+                        <div className="check">
+                        <input type="checkbox" name="frutales" id="frutales" /><label htmlFor="frutales">frutales</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="florales" id="florales" />
+                        <label htmlFor="florales">florales</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="herbales" id="herbales" />
+                        <label htmlFor="herbales">herbales</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="maderosas" id="maderosas" />
+                        <label htmlFor="maderosas">maderosas</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="sensuales" id="sensuales" />
+                        <label htmlFor="sensuales">sensuales</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="especiadas" id="especiadas" />
+                        <label htmlFor="especiadas">especiadas</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="dulces" id="dulces" />
+                        <label htmlFor="dulces">dulces</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="cítricas" id="cítricas" />
+                        <label htmlFor="cítricas">cítricas</label>
+                        </div>
+                        <div className="check">
+                        <input type="checkbox" name="balsamos" id="balsamos" />
+                        <label htmlFor="balsamos">balsamos</label>
+                        </div>
+                    </div>
             </div>
-                <div className={filter?'w-3/4 grid grid-cols-2 gap-x-24 gap-y-14 pt-14':'grid grid-cols-3 gap-x-24 gap-y-14 pt-14'}>
+                <div className={filter?'w-3/4 grid md:grid-cols-2 grid-cols-1 gap-x-24 gap-y-14 pt-14':'grid md:grid-cols-3 grid-cols-2 md:gap-x-24 gap-x-7 md:gap-y-14 gap-y-4 md:pt-14 pt-6'}>
             {currentProducts?.map((product, index) => (
       <div key={index}><Link to={`/products/${index + 1}`}> <img src={product.image} alt="" /></Link></div>
     ))}        
