d42d350f7a8ee980245c5ec43399de29152a1e38
Md Joshim Uddin <mju881@gmail.com>
2024-06-06 21:32:52 +0600

    Button hover updated

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = (props) => {
     return (
-        <button className='cta'>{props.text}</button>
+        <button className='cta hover:bg-white hover:text-black hover:duration-300'>{props.text}</button>
     );
 };
 
